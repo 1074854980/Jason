2ed1dca613d97fb412e2fb69dbfa33aa6cb895ef
jason <1074854980@qq.com>
2019-08-23 15:11:47 +0800

    提交新功能

--- Goods.java
+++ Goods.java
@@ -1,4 +1,14 @@
 public class Goods{
 	private int gid;
 	private String gname;
+	
+	public void setGid(int gid){
+		this.gid = gid;
+	}
+	public gid getGid(){
+		return gid;
+	}
+	public void setGname(String gname){
+		this.gname=gname
+	}
 }
